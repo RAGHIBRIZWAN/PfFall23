dea96ca4655c93e384dc234026b9c9b2e6263c25
RAGHIBRIZWAN <142868123+RAGHIBRIZWAN@users.noreply.github.com>
2023-09-05 10:42:32 +0500

    Update EX03.c

--- labs/03/EX03.c
+++ labs/03/EX03.c
@@ -1,1 +1,20 @@
+#RAGHIB RIZWAN RABANI
+##23k-0012 B-AI
 
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+	int perp;
+	perp = 0;
+	printf("ENTER THE VALUE OF PERPENDICULAR:");
+	scanf("%d",&perp);
+	int base;
+	base = 0;
+	printf("ENTER THE VALUE OF BASE:");
+	scanf("%d",&base);
+	int p = perp*perp;
+	int b = base*base;
+	printf("HYPOTENUSE: %f",sqrt(p+b));
+	return 0; 
+}
