5d82799b9fe13539317b9cd35e30ef18354af2cf
RAGHIBRIZWAN <142868123+RAGHIBRIZWAN@users.noreply.github.com>
2023-09-05 10:26:02 +0500

    Update Ex02.c

--- labs/03/Ex02.c
+++ labs/03/Ex02.c
@@ -1,1 +1,23 @@
-
+#include <stdio.h>
+int main(){
+	int a;
+	a = 0;
+	int b;
+	b = 0;
+	int c;
+	c = 0;
+	
+	printf("ENTER LENGTH:");
+	scanf("%d",&a);
+	
+	printf("ENTER WIDTH:");
+	scanf("%d",&b);
+	
+	printf("ENTER HEIGHT:");
+	scanf("%d",&c);
+	
+	int m;
+	m = a*b*c;
+	printf(" %d",m);
+	return 0;
+}
