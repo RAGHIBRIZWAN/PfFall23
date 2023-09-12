4843f9af257abbd11306707c7963adc70120e00b
RAGHIBRIZWAN <142868123+RAGHIBRIZWAN@users.noreply.github.com>
2023-09-12 10:54:33 +0500

    Update q01.c

--- labs/04/q01.c
+++ labs/04/q01.c
@@ -1,1 +1,27 @@
+/*
+* Programmer: Raghib Rizwan Rabani
+* Date: 12/09/2023
+*/
 
+# include <stdio.h>
+
+int main(){
+	int n;
+	n = 0;
+	printf("Input any number");
+	scanf("%d", &n);
+	int i;
+	i = 1;
+	while (i <= n){
+		int r = i;
+		i = i + 1;
+		while(r>0){
+			r = r-1;
+			
+			printf("*", &r);
+			
+		}
+		printf("\n");		
+	}
+	
+}
