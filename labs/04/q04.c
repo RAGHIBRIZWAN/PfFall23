c44bfd12a7f4d9c16a245f2780cf6d7b06e104c8
RAGHIBRIZWAN <142868123+RAGHIBRIZWAN@users.noreply.github.com>
2023-09-12 21:30:05 +0500

    Create q04.c

---
+++ labs/04/q04.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    int a,b,c;
+    a=0;
+    b=0;
+    c=0;
+    printf("ENTER VALUE OF a:");
+    scanf("%d",&a);
+    printf("ENTER VALUE OF b:");
+    scanf("%d",&b);
+    printf("ENTER VALUE OF c:");
+    scanf("%d",&c);
+    int sqrt1 = (b*b - 4*a*c);
+    if(sqrt1 > 0){
+        float r1 = (-b + sqrt(sqrt1)) / (2 * a);
+        float r2 = (-b - sqrt(sqrt1)) / (2 * a);
+          printf("%f",r1);
+          printf("%f\n",r2);
+    }
+  
+    else if(sqrt1 == 0){
+        float r1 = (-b / (2 * a));
+        printf("%f",r1);
+    }
+    else if (sqrt1 < 0){
+        printf("Negative Number in square root");
+    }
+   else{
+       printf("ERROR");
+   }
+   
+}
