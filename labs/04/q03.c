ec99b2f5e90aa1ee00287790d4a2d5d4f9418047
RAGHIBRIZWAN <142868123+RAGHIBRIZWAN@users.noreply.github.com>
2023-09-12 20:39:37 +0500

    Create q03.c

---
+++ labs/04/q03.c
@@ -0,0 +1,24 @@
+
+
+
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
+    
+    int i = 0;
+    
+    while(i<n) {
+        i = i+1;
+        int j = 0;
+        while (j<n)  {
+            j = j+1;
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
