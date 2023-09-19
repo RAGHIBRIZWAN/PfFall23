63ba8042f3fd06e2301e447015a5b3fecd1a165c
RAGHIBRIZWAN <142868123+RAGHIBRIZWAN@users.noreply.github.com>
2023-09-19 19:41:08 +0500

    Create t8.c

---
+++ labs/05/t8.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void main() {
+        int a = 0;
+        printf("Enter valur from 0-1000: ");
+        scanf("%d", &a);
+        if(a > 500){
+            printf("HIGH BRIGHTNESS");
+        }
+        else if(a >=0 && a<=100){
+            printf("It is evening time so moderate brightness");
+        }
+        else if("a >= 101 && a<= 500"){
+            printf("Lighting Low Brightness");
+        }
+        else{
+            printf("ERROR");
+        }
+}
