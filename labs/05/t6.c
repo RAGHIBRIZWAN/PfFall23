4bc49155c27b6d7ba572bc3e9244728c30728737
RAGHIBRIZWAN <142868123+RAGHIBRIZWAN@users.noreply.github.com>
2023-09-19 10:03:04 +0500

    Create t6.c

---
+++ labs/05/t6.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+void main(){
+	int a=0,b=0,c=0,d=0,e=0,f=0,g=0,h=0;
+
+	printf("ENTER ANY DIGIT NUMBER IN 0s AND 1s: ");
+	scanf("%d",&a);
+
+	printf("ENTER ANY DIGIT NUMBER IN 0s AND 1s: ");
+	scanf("%d",&b);
+
+	printf("ENTER ANY DIGIT NUMBER IN 0s AND 1s: ");
+	scanf("%d",&c);
+	
+	printf("ENTER ANY DIGIT NUMBER IN 0s AND 1s: ");
+	scanf("%d",&d);
+	
+	printf("ENTER ANY DIGIT NUMBER IN 0s AND 1s: ");
+	scanf("%d",&e);
+	
+	printf("ENTER ANY DIGIT NUMBER IN 0s AND 1s: ");
+	scanf("%d",&f);
+	
+	printf("ENTER ANY DIGIT NUMBER IN 0s AND 1s: ");
+	scanf("%d",&g);
+	
+	printf("ENTER ANY DIGIT NUMBER IN 0s AND 1s: ");
+	scanf("%d",&h);
+	
+
+	if(d == 1 && g == 1){
+		printf("bits are off \n");
+		printf("%d",d = 0);
+	}
+	else{
+		printf("ALL IS OK");
+	}
+}
