8aa561013ca26f1da1d9d0db94ed86d3566cbf91
RAGHIBRIZWAN <142868123+RAGHIBRIZWAN@users.noreply.github.com>
2023-09-20 15:13:14 +0500

    Update t6.c

--- labs/05/t6.c
+++ labs/05/t6.c
@@ -1,39 +1,17 @@
 #include <stdio.h>
 
-void main(){
-	int a=0,b=0,c=0,d=0,e=0,f=0,g=0,h=0;
-
-	printf("ENTER ANY DIGIT NUMBER IN 0s AND 1s: ");
-	scanf("%d",&a);
-
-	printf("ENTER ANY DIGIT NUMBER IN 0s AND 1s: ");
-	scanf("%d",&b);
-
-	printf("ENTER ANY DIGIT NUMBER IN 0s AND 1s: ");
-	scanf("%d",&c);
-	
-	printf("ENTER ANY DIGIT NUMBER IN 0s AND 1s: ");
-	scanf("%d",&d);
-	
-	printf("ENTER ANY DIGIT NUMBER IN 0s AND 1s: ");
-	scanf("%d",&e);
-	
-	printf("ENTER ANY DIGIT NUMBER IN 0s AND 1s: ");
-	scanf("%d",&f);
-	
-	printf("ENTER ANY DIGIT NUMBER IN 0s AND 1s: ");
-	scanf("%d",&g);
-	
-	printf("ENTER ANY DIGIT NUMBER IN 0s AND 1s: ");
-	scanf("%d",&h);
-	
-
-	if(d == 1 && g == 1){
-		printf("bits are off \n");
-		printf("%d",d = 0);
-		printf("%d",g=0);
-	}
-	else{
-		printf("ALL IS OK");
-	}
+void main() {
+    int num, bit7, bit4;
+    printf("ENTER 8 NUMBERS IN 0s AND 1s: ");
+    scanf("%d",&num);
+    
+    bit4= (num / 1000) % 10;
+    bit7 = (num / 1000000) % 10;
+    
+    if (bit4 && bit7) {
+        num = num - 1000000 - 1000;
+    }
+    
+    
+    printf("%08d\n", num);
 }
