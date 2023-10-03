3631c9edb791994fd1cea642d3e8be8b0dae478f
RAGHIBRIZWAN <142868123+RAGHIBRIZWAN@users.noreply.github.com>
2023-10-03 09:34:50 +0500

    Create task2.c

---
+++ labs/lab06/task2.c
@@ -0,0 +1,30 @@
+/*
+* Programmer = Raghib Rizwan Rabani
+* Roll no: = 23K0012
+* Desc: Checking if a number is perfect number or not
+*/
+
+
+
+#include <stdio.h>
+
+void main(){
+
+	int a = 0;
+	int b = 1;
+	int sum = a + b;
+	int sum2 = 0;
+
+	for(int i = 0;b<=10000;i++){
+		a = b;
+		b = sum;
+		sum = a + b;
+		printf("%d, ",a);
+
+		if(a%3 == 0 || a%5 == 0 || a%7 == 0){
+			sum2 += a;
+			
+		}
+	}
+	printf("The sum of numbers divisible by are: %d",sum2);
+}
