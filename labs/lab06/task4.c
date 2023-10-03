ca4b92cd1f0e18b203ff30f263bc31d195f5422f
RAGHIBRIZWAN <142868123+RAGHIBRIZWAN@users.noreply.github.com>
2023-10-03 11:00:13 +0500

    Create task4.c

---
+++ labs/lab06/task4.c
@@ -0,0 +1,39 @@
+/*
+* Programmer = Raghib Rizwan Rabani
+* Roll no: = 23K0012
+* Desc: Checking if a number is perfect number or not
+*/
+
+#include <stdio.h>
+
+void main(){
+	int a = 0;
+	int b = 0;
+	printf("Enter 1st number: ");
+	scanf("%d",&a);
+
+	printf("Enter 2nd number: ");
+	scanf("%d",&b);
+
+	for(int i = a;i<=b;++i){
+		switch(i){
+		case 0:printf("zero\n");break;
+		case 1:printf("one\n");break;
+		case 2:printf("two\n");break;
+		case 3:printf("three\n");break;
+		case 4:printf("four\n");break;
+		case 5:printf("five\n");break;
+		case 6:printf("six\n");break;
+		case 7:printf("seven\n");break;
+		case 8:printf("eight\n");break;
+		case 9:printf("nine\n");break;
+		default:if(i%2==0){
+				printf("EVEN\n");
+			}
+			else{	
+				printf("ODD");
+			}
+		}
+	
+	}
+}
