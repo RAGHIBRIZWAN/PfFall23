4da1c0596ba3f23241d1157cd72c320a1151835e
RAGHIBRIZWAN <142868123+RAGHIBRIZWAN@users.noreply.github.com>
2023-10-07 13:33:33 +0500

    Update task7.c

--- labs/lab06/task7.c
+++ labs/lab06/task7.c
@@ -1,16 +1,22 @@
+/*
+* Programmer = Raghib Rizwan Rabani
+* Roll no: = 23K0012
+* Desc: Sum of numbers in Array.
+*/
+
 #include <stdio.h>
 
 int main() {
-    int arr[10] = {};
-    int i = 0;
-    int a = 0;
+    int arr[10] = {}; //declaration and initialization
+    int i = 0; //declaration and initialization
+    int a = 0; //declaration and initialization
     while(i<=9){
     printf("Enter the value:");
     scanf("%d",&arr[i]);
     a += arr[i];
     i++;
-    }
+    } //end while
     printf("The sum of the value is: %d",a);
     
     return 0;
-}
+} //end main
