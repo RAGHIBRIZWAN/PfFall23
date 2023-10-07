fc023c38f153ad570c850fe3a2ab5b932fdd33b0
RAGHIBRIZWAN <142868123+RAGHIBRIZWAN@users.noreply.github.com>
2023-10-07 13:34:43 +0500

    Update task8.c

--- labs/lab06/task8.c
+++ labs/lab06/task8.c
@@ -1,18 +1,20 @@
+
+
 #include <stdio.h>
 
 int main() {
-    int arr[10] = {};
-    int i = 0;
+    int arr[10] = {}; //declaration and initialization
+    int i = 0; //declaration and initialization
     while(i<=9){
     printf("Enter the value: ");
     scanf("%d",&arr[i]);
     i++;
-    }
+    } //end while
     i = 9;
     while(i >= 0){
         printf("%d\t",arr[i]);
         --i;
-    }
+    } //end while
     
     return 0;
-}
+} //end main
