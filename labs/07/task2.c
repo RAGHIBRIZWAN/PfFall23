91a6f62b40ddbc2da9ca887a3b342b68a83bb68f
RAGHIBRIZWAN <142868123+RAGHIBRIZWAN@users.noreply.github.com>
2023-10-10 09:27:14 +0500

    Create task2.c

---
+++ labs/07/task2.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(){
+	int arr[10] = {0};
+	int d = 0;
+	int N;
+	int i = 0;
+	printf("Enter vale of N <= 10: ");
+	scanf("%d",&N);
+	for(i=0; i<N; i++){
+		printf("Enter an integer in array");
+		scanf("%d",&arr[i]);
+	}
+	
+	printf("Enter value of d: ");
+	scanf("%d",&d);
+
+	for(int j = 0; j < d; j++){
+		for(i = 0; i<N - 1; i++){
+			int temp = arr[i+1];
+			arr[i+1] = arr[i];
+			arr[i] = temp;
+		}
+	}
+
+	for(i=0; i<N; i++){
+	printf("%d",arr[i]);
+	}
+}
