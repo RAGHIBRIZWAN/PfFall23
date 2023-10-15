46d4d7616b5a320f34bd004c0734d0f072f6981e
RAGHIBRIZWAN <142868123+RAGHIBRIZWAN@users.noreply.github.com>
2023-10-15 10:52:04 +0500

    Create task8.c

---
+++ labs/07/task8.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main() {
+    int arr[10] = {0};
+    int size = 0;
+    int a = 0;
+    int i = 0;
+    int j = 0;
+    int temp;
+    
+    printf("Enter size of Array: ");
+    scanf("%d",&size);
+    
+    for(i=0; i<size; i++){
+		printf("Enter an integer in array");
+		scanf("%d",&arr[i]);
+	}
+    
+    for(i = 0; i < size; i++){
+        for(j = i+1; j < size; j++){
+            if(arr[i] > arr[j]){
+                temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+    for(i = 0; i < size; i++){
+        printf("%d",arr[i]);
+    }
+
+    return 0;
+}
