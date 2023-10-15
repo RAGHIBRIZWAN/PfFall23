f0635a090d35172792ea1c08ddce9fa92deb4fcf
RAGHIBRIZWAN <142868123+RAGHIBRIZWAN@users.noreply.github.com>
2023-10-15 12:46:03 +0500

    Update task10.c

--- labs/07/task10.c
+++ labs/07/task10.c
@@ -1,78 +1,35 @@
+/*programmer:Syed Fahad Faheem Shah(23k-0062)
+  Description: Taking an unsorted array by user that contains only + integers, find a continuous sub-
+array which adds to a given number S.
+  date:10/10/2023
+ */
 #include <stdio.h>
-
-int main() {
-    int S = 0;
-    int size = 0;
-    int i = 0;
-    int arr[10] = {0};
-    //int arr[5] = {1,2,3,7,5};
-    int j = 0;
-    int k = 0;
-    int l = 0;
-    int m = 0;
-    int ans = 0;
-    int temp;
+    int main() {
+    int sum, num,hold=0,startingindex,endindex;
+    printf("Enter Number of elements");
+    scanf("%d", &num);
+    int arr[num];
+    printf("\nEnter Elements:");
     
-    for(i=0; i<5; i++){
-		printf("Enter an integer in array");
-		scanf("%d",&arr[i]);
-	}
-	
-	printf("Enter value of S: ");
-    scanf("%d",&S);
-
-    for(i = 0; i < 5; i++){
-        for(j = i+1; j < 5; j++){
-            temp = arr[i] + arr[j];
-            if(temp == S){
-                ans = temp;
+    // taking input elements and sum
+    for(int i=0;i<num;i++){
+    scanf("%d", &arr[i]);
+    }
+    printf("\nEnter Sum:");
+    scanf("%d", &sum);
+     
+    for(int i=0;i<num;i++){
+        hold=0;
+        for(int j=i+1;j<num;j++){
+            hold += arr[j];
+            if(hold == sum){
+                startingindex = i+1;
+                endindex = j;
+                break;
             }
         }
     }
-    
-    for(i = 0; i < 5; i++){
-        for(j = i+1; j < 5; j++){
-            for(k = j+1; k < 5; k++){
-                temp = arr[i] + arr[j] + arr[k];
-                if(temp == S){
-                    ans = temp;
-                }
-            }
-        }
-    }
-    
-    for(i = 0; i < 5; i++){
-        for(j = i+1; j < 5; j++){
-            for(k = j+1; k < 5; k++){
-                for(l = k+1; l<5; l++){
-                    temp = arr[i] + arr[j] + arr[k] + arr[l];
-                    if(temp == S){
-                        ans = temp;
-                    }
-                }
-            }
-        }
-    }
-    
-    for(i = 0; i < 5; i++){
-        for(j = i+1; j < 5; j++){
-            for(k = j+1; k < 5; k++){
-                for(l = k+1; l<5; l++){
-                    for(m = l+1; m<5; m++){
-                        temp = arr[i] + arr[j] + arr[k] + arr[l] + arr[m];
-                        if(temp == S){
-                            ans = temp;
-                        }
-                    }
-                }
-            }
-        }
-    }
-    
-
-    
-    printf("%d",ans);
-    
-
+   
+   printf("The Elements from Index %d to %d when summed results in the output of %d.", startingindex,endindex,sum);
     return 0;
 }
