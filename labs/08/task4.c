924227e7b73fdf92da822b3dde83adf44afb8581
RAGHIBRIZWAN <142868123+RAGHIBRIZWAN@users.noreply.github.com>
2023-10-24 10:33:46 +0500

    Update task4.c

--- labs/08/task4.c
+++ labs/08/task4.c
@@ -1,3 +1,33 @@
+/*
+  Programmer: Raghib Rizwan Rabani 
+  Roll No: 23K-0012
+  Date: 24 oct 2023
+  Desc:
+Your task is to create a C program that performs password length
+validation and user authentication based on the given requirements below:
+
+
+a)    
+Prompts the user to enter a password.
+
+
+b)    
+Validates the entered password by checking if it is
+at least 8 characters long.
+
+
+c)    
+If the password meets the length requirement, the
+program should compare it to a stored password "Secure123."
+
+
+d)    
+If the
+entered password matches the stored password, display "Login successful.
+Welcome!" Otherwise, display "Login failed. Incorrect password."
+
+*/
+
 #include <stdio.h>
 #include <string.h>
 
