c9c905838053a67231a0c0ac0a69806ccb249e25
RAGHIBRIZWAN <142868123+RAGHIBRIZWAN@users.noreply.github.com>
2023-10-24 10:35:12 +0500

    Update task6.c

--- labs/08/task6.c
+++ labs/08/task6.c
@@ -1,3 +1,31 @@
+/*
+  Programmer: Raghib Rizwan Rabani 
+  Roll No: 23K-0012
+  Date: 24 oct 2023
+  Desc:You are given an array of integers. Write a C program that
+defines a user-defined function processArray to calculate the sum, maximum, and
+minimum values in the array. The program should take the array and its size as
+parameters and use the function to compute these values.
+
+
+Input:Array is [4, 8, 1, 15, 6]
+
+
+Output:
+
+
+Array Sum:
+34
+
+
+Maximum
+Value: 15
+
+
+Minimum Value: 1
+
+    */
+
 #include <stdio.h>
 
 int processArray(int brr[],int n){
