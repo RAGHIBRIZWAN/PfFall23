acb3cca19d37dd84346b331d1f1d5b76227e209b
RAGHIBRIZWAN <142868123+RAGHIBRIZWAN@users.noreply.github.com>
2023-10-24 10:34:39 +0500

    Update task5.c

--- labs/08/task5.c
+++ labs/08/task5.c
@@ -1,3 +1,16 @@
+/*
+  Programmer: Raghib Rizwan Rabani 
+  Roll No: 23K-0012
+  Date: 24 oct 2023
+  Desc:In this C program, you are tasked with creating a function
+called decideCarUsage that helps users decide whether they should use their car
+on a particular day of the week. Users provide the numeric part of their car's
+number and the day of the week (1 to 7). The program applies a simple rule:
+even-numbered cars should be used on even days, and odd-numbered cars on odd
+days. The function returns 1 if the car should be used and 0 if it should not.
+    */
+
+#include <stdio.h>
 int decidecarusage(int a, int b){
     if(a%2 == b%2){
         return 1;
