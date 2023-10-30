647fb3176a8441f1d12e560dc131429a09b8de23
RAGHIBRIZWAN <142868123+RAGHIBRIZWAN@users.noreply.github.com>
2023-10-30 20:42:30 +0500

    Update Task5.c

--- Assignments/Assignment 2/Task5.c
+++ Assignments/Assignment 2/Task5.c
@@ -1,25 +1,26 @@
-#include<stdio.h>
-#include<math.h>
-int main (){
+#include <stdio.h>
+#include <math.h>
+int main(){
     int n;
-    printf("Enter number : ");
-    scanf("%d", &n);
-    
-    for (int i=1; i<n*n*n;i++) {
-            
-        for (int j = 1; j <n*n*n; j++) {
-            
-            for (int k=j+1; k<n*n*n;k++) {
-            
-                for (int l = k + 1; l < n*n*n; l++) {
-                    if(i*i*i + j*j*j == k*k*k + l*l*l  ){
-                        printf("%d=%d^3+%d^3=%d^3+%d^3\n", i*i*i+j*j*j ,i,j,k,l);
-                    
+    int a,b,c,d,f,g;
+    printf("enter limit n^3: ");
+    scanf("%d",&n);
+    for(a=1;a<=n*n*n;a++){
+        for(b=1;b<=n*n*n;b++){
+             f = a*a*a + b*b*b;
+            for(c=b+1;c<=n*n*n;c++){
+                for(d=c+1;d<=n*n*n;d++){
+                    g= d*d*d + c*c*c;
+                    if(f==g){
+                        printf("Ramanujan numbers");
+                        printf("%d^3 + %d^3 == %d^3 + %d^3 = %d",a,b,c,d,a*a*a+b*b*b);
+                        printf("\n");
                     }
                 }
             }
         }
     }
-
+    
+    
     return 0;
 }
