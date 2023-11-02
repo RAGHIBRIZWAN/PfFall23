94fa5de0e80f61837a44f3e524b992f013ddf5ce
RAGHIBRIZWAN <142868123+RAGHIBRIZWAN@users.noreply.github.com>
2023-11-02 22:59:43 +0500

    Update Task4.c

--- labs/09/Task4.c
+++ labs/09/Task4.c
@@ -1,1 +1,22 @@
-N * fact(N-1)
+#include <stdio.h>
+
+int fact(int n){
+    if(n==1){
+        return 1;
+    }
+    else{
+        return n*fact(n-1);
+    }
+    return n;
+}
+
+int main() {
+    int n,temp;
+    printf("Enter the number n: ");
+    scanf("%d",&n);
+    
+    int ans = fact(n);
+    printf("%d",ans);
+
+    return 0;
+}
