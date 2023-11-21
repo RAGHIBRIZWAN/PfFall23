2750d48e904c7206898bd6abdf6f0f263eef92a5
RAGHIBRIZWAN <142868123+RAGHIBRIZWAN@users.noreply.github.com>
2023-11-21 21:51:16 +0500

    Update Task6.c

--- labs/11/Task6.c
+++ labs/11/Task6.c
@@ -1,33 +1,30 @@
-// Online C compiler to run C program online
 #include <stdio.h>
+#include <string.h>
 
-struct size{
-    int height;
-    int length;
-    int width;
+struct emp{
+    int emp_id;
+    char emp_name[20];
+    int emp_sal;
+};
+struct org{
+    int size;
+    char org_name[20];
+    char org_number[20];
+    struct emp data[50];
 };
 
-int main() {
-    struct size s[10];
-    int width = 100000;
-    int height = 41;
-    int length = 100000;
-    
-    printf("Enter the width: ");
-    scanf("%d",&s[0].width);
-    printf("Enter the length: ");
-    scanf("%d",&s[0].length);
-    printf("Enter the height: ");
-    scanf("%d",&s[0].height);
-    
-    int v = s[0].length*s[0].width*s[0].height;
-    
-    if(s[0].height < height){
-        printf("The volume is: %d",v);
-    }
-    else{
-        printf("Impossible");
-    }
 
+int main() {
+    struct org d[50];
+    
+    d[0].size = 123;
+    strcpy(d[0].org_name, "NU-FAST");
+    strcpy(d[0].org_number, "NUFAST123ABC");
+    d[0].data[0].emp_id = 127;
+    strcpy(d[0].data[0].emp_name, "Linus Sebastian");
+    d[0].data[0].emp_sal = 400000;
+    
+    printf("The size of structure organization: %d\n Organization name: %s\n Organization number: %s\n Employee id: %d\n Employee name: %s\n Employee salary: %d",d[0].size,d[0].org_name,d[0].org_number,d[0].data[0].emp_id,d[0].data[0].emp_name,d[0].data[0].emp_sal);
+    
     return 0;
 }
