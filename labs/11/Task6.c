4dd9ead21b9d77a23f77a1aa0ee7c5a1a1139a55
RAGHIBRIZWAN <142868123+RAGHIBRIZWAN@users.noreply.github.com>
2023-11-21 22:08:40 +0500

    Update Task6.c

--- labs/11/Task6.c
+++ labs/11/Task6.c
@@ -1,30 +1,33 @@
 #include <stdio.h>
 #include <string.h>
 
-struct emp{
-    int emp_id;
-    char emp_name[20];
-    int emp_sal;
-};
-struct org{
-    int size;
-    char org_name[20];
-    char org_number[20];
-    struct emp data[50];
+struct date{
+    int day;
+    int month;
+    int year;
 };
 
 
 int main() {
-    struct org d[50];
+    struct date d[50];
     
-    d[0].size = 123;
-    strcpy(d[0].org_name, "NU-FAST");
-    strcpy(d[0].org_number, "NUFAST123ABC");
-    d[0].data[0].emp_id = 127;
-    strcpy(d[0].data[0].emp_name, "Linus Sebastian");
-    d[0].data[0].emp_sal = 400000;
+    d[0].day = 21;
+    d[0].month = 11;
+    d[0].year = 2023;
     
-    printf("The size of structure organization: %d\n Organization name: %s\n Organization number: %s\n Employee id: %d\n Employee name: %s\n Employee salary: %d",d[0].size,d[0].org_name,d[0].org_number,d[0].data[0].emp_id,d[0].data[0].emp_name,d[0].data[0].emp_sal);
+    d[0].day += 45;
+    
+    while(d[0].day > 30){
+        d[0].day -= 30;
+        d[0].month++;
+    }
+    
+    if(d[0].month > 12){
+        d[0].month = 1;
+        d[0].year++;
+    }
+    
+    printf("Final Date is: %d %d %d",d[0].day,d[0].month,d[0].year);
     
     return 0;
 }
